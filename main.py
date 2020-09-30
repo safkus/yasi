4872dce3569822d465bf836b0bed108cda4482e0
ml4711 <markus.lanser@gmail.com>
2020-09-30 20:14:20 +0100

    setting up main window and main function

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os
 import time
 import random
 
+WIDTH = 750
+HEIGHT = 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Yet Another Space Invaders")
+
 # Load enemy ship image assets
 RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.png"))
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
@@ -19,3 +24,17 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 
 # Background image
 BG = pygame.image.load(os.path.join("assets", "background-black.png"))
+
+def main():
+	run = True
+	FPS = 60
+	clock = pygame.time.Clock()
+
+	while run:
+		clock.tick(FPS)
+
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				run = False
+
+main()
