958059b34bb55c7fd6893a2786131b5df49f7036
ml4711 <markus.lanser@gmail.com>
2020-10-01 19:27:31 +0100

    adding user controls with wasd

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import os
 import time
 import random
+pygame.font.init()
 
 WIDTH = 750
 HEIGHT = 750
@@ -26,13 +27,44 @@ YELLOW_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_yellow.png"
 BG = pygame.image.load(os.path.join("assets", "background-black.png"))
 BG = pygame.transform.scale(BG,(WIDTH,HEIGHT))
 
+class Ship:
+	def __init__(self, x, y, health=100):
+		self.x = x
+		self.y = y
+		self.health = health
+		self.ship_img = None
+		self.laser_img = None
+		self.lasers = []
+		self.cool_down_counter = 0
+
+	def draw(self, window):
+		pygame.draw.rect(window, (255,0,0), (self.x, self.y, 50, 50))
+
 def main():
 	run = True
 	FPS = 60
+	level = 1
+	lives = 5
+	main_font = pygame.font.SysFont("comicsans",50)
+
+	player_vel = 5
+
+	ship = Ship(300,650)
+
 	clock = pygame.time.Clock()
 
 	def redraw_window():
 		WIN.blit(BG, (0,0))
+
+		# draw text
+		lives_label = main_font.render(f"Lives: {lives}", 1, (255,255,255))
+		level_label = main_font.render(f"Level: {level}", 1, (255,255,255))
+
+		WIN.blit(lives_label, (10, 10))
+		WIN.blit(level_label, (WIDTH - level_label.get_width() - 10, 10))
+
+		ship.draw(WIN)
+
 		pygame.display.update()
 
 	while run:
@@ -42,5 +74,15 @@ def main():
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				run = False
+
+		keys = pygame.key.get_pressed()
+		if keys[pygame.K_a]: # left
+			ship.x -= player_vel
+		if keys[pygame.K_d]: # right
+			ship.x += player_vel
+		if keys[pygame.K_w]: # up
+			ship.y -= player_vel
+		if keys[pygame.K_s]: # down
+			ship.y += player_vel
 
 main()
